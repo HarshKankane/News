b647a28fc69ffebbe8608d835568875063b5e4c5
harshkankane <harshkankane2002@gmail.com>
2024-11-03 11:03:00 +0530

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LazyLoading from './LazyLoading'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Learning Lazy loading in react</h1>
+       
+    </div>
+  );
+}
+
+export default App
+  
